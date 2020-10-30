f5c3621f160f271988a96918d4b922a95b1f30a9
Ashly Mayle <infiernodeashly@gmail.com>
2020-10-30 12:12:44 -0400

    update

--- index.js
+++ index.js
@@ -69,9 +69,7 @@ const questions = [
         type: "list",
         message: "Choose a license type",
         choices: ['Apache License 2.0', 'Boost Software License 1.0', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'MIT License', 'Mozilla Public License 2.0', 'The Unlicense'],
-        var img1 = document.createElement("img");
-            img1.src = `<https://img.shields.io/badge/license-${data.choices}-blue`;
-       
+               
     },
 
         // GitHub username question
@@ -95,7 +93,6 @@ const questions = [
 // function to write README file
 async function writeToFile(fileName, data) {
     let readMeTemplate = `# ${data.title}
-
 
   
 ## Description 
@@ -121,7 +118,7 @@ ${data.install}
 
 ${data.usage}
 
-## License ##Badge ${data.badge}
+## License ##Badge 
 
 ${data.license}
 
